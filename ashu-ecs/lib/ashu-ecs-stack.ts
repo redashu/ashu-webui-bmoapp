8dede18e89566d1e2d36bb619d184c05443780e8
redashu <ashutoshh@linux.com>
2024-05-31 00:09:17 +0530

    cdk check

---
+++ ashu-ecs/lib/ashu-ecs-stack.ts
@@ -0,0 +1,42 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2'
+import * as ecs from  'aws-cdk-lib/aws-ecs'
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class AshuEcsStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const vpc = ec2.Vpc.fromLookup(this,'ashuExistingVpc', {
+      vpcId: 'vpc-063afa0c24ec80cb8'
+    });
+    // defining ECS cluster info 
+    const cluster = new ecs.Cluster(this,'ashu-ecs-cluster',{
+      clusterName: 'ashu-ecs-bycdk', // change cluster name 
+      vpc: vpc,
+      enableFargateCapacityProviders: true ,
+      containerInsights: true // enable cloudwatch monitoring 
+    });
+    // // add ec2 capacity 
+    // cluster.addCapacity('defaultashuScaleGroup',{
+    //   instanceType: new ec2.InstanceType("t2.small"),
+    //   desiredCapacity: 1, // container instances
+    //   minCapacity: 1,
+    //   maxCapacity: 5
+    // });
+
+    // task Definition of farget launch type 
+    const ashuTaskDef = new ecs.FargateTaskDefinition(this,'ashu-frg-task1',{
+      cpu:  256,
+      memoryLimitMiB: 512
+       
+    });
+    // adding container info 
+    const container = ashuTaskDef.addContainer('ashucdkc1',{
+      image: ecs.ContainerImage.fromRegistry('dockerashu/ashubmo:nginxuiv1'),
+      memoryLimitMiB: 256,
+      portMappings: [{ containerPort: 80 }]
+    });
+  }
+}
---
+++ ashu-ecs/test/ashu-ecs.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AshuEcs from '../lib/ashu-ecs-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ashu-ecs-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AshuEcs.AshuEcsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
