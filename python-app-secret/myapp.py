b780a15463ab54bde124085d1f2a3eb3ef66245e
redashu <ashutoshh@linux.com>
2024-05-30 19:47:35 +0530

    udpates python app

---
+++ python-app-secret/myapp.py
@@ -0,0 +1,10 @@
+import time
+
+while True:
+    print("Hello all , welcome to python..!!")
+    time.sleep(3)
+    print("Welcome to BMO..plz wait will be using secrets here ")
+    time.sleep(2)
+    print("Welcome to Containers & cicd..!!")
+    print("______________________")
+    time.sleep(3)
