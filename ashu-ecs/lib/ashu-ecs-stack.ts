9f57810a3870f7f746f2bbf875f3d3157d4b6991
redashu <ashutoshh@linux.com>
2024-05-31 22:12:35 +0530

    udpates cdk

--- ashu-ecs/lib/ashu-ecs-stack.ts
+++ ashu-ecs/lib/ashu-ecs-stack.ts
@@ -25,7 +25,11 @@ export class AshuEcsStack extends cdk.Stack {
     //   minCapacity: 1,
     //   maxCapacity: 5
     // });
-
+    // defingin container image tag as variable while doing cdk deploy we will pass this info 
+    const imageTag = this.node.tryGetContext('imageTag');
+    if (!imageTag) {
+      throw new Error('context variable name is required');
+    }
     // task Definition of farget launch type 
     const ashuTaskDef = new ecs.FargateTaskDefinition(this,'ashu-frg-task1',{
       cpu:  256,
@@ -34,9 +38,25 @@ export class AshuEcsStack extends cdk.Stack {
     });
     // adding container info 
     const container = ashuTaskDef.addContainer('ashucdkc1',{
-      image: ecs.ContainerImage.fromRegistry('dockerashu/ashubmo:nginxuiv1'),
+      image: ecs.ContainerImage.fromRegistry(`dockerashu/ashubmoweb:bmov${imageTag}`),
       memoryLimitMiB: 256,
       portMappings: [{ containerPort: 80 }]
+    });
+    // creating security group 
+    const ashusecgroup = new ec2.SecurityGroup(this,'ashufirewallgrp',{
+      vpc: vpc,
+      description: 'allow ingress rules for 80 port'
+    });
+    ashusecgroup.addIngressRule(ec2.Peer.anyIpv4(),ec2.Port.tcp(80),'allow http traffic');
+    // creating service using above task defintion 
+
+    const service = new ecs.FargateService(this,'ashuECSserviceCDK',{
+      cluster,
+      taskDefinition: ashuTaskDef,
+      serviceName: 'ashu-svc-bycdk',
+      desiredCount: 2,
+      assignPublicIp: true,
+      securityGroups: [ashusecgroup]   // attaching security group 
     });
   }
 }
